eb7fac7f4d78a10a598a0537b71eb35aa3c2764d
Menna Adel <adelmenna68@gmail.com>
2024-09-06 16:54:52 +0300

    first commit

---
+++ scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    // Custom jQuery for interactive elements
+    $('.navbar-toggler').click(function() {
+        $('.navbar-collapse').slideToggle();
+    });
+});
